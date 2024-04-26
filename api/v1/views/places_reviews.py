cf5ba36cc962b350691cda499b2ffa909ad58eaf
annmulwa <annmulwa2000@gmail.com>
2024-04-26 11:24:41 -0700

    reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -11,7 +11,7 @@ from api.v1.views import app_views
 
 
 @app_views.route('/places/<place_id>/reviews', strict_slashes=False)
-def get_all_reviews(city_id):
+def get_all_reviews(place_id):
     """
     Retrieves the list of all Review objects of a Place
     """
