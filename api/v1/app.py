a3bddd44216340b6296b239b9a16074b6de144b9
annmulwa <annmulwa2000@gmail.com>
2024-04-28 23:24:06 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,13 @@ Starting my API
 """
 from os import getenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 
 app.register_blueprint(app_views)
 
